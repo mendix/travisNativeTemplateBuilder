6fd5b3cef07632b8cdfe75292d2ea915ceb04378
mehmet <mehmet.izci@mendix.com>
2020-10-06 16:33:09 +0200

    remove set -e since android wait will throw

--- android-wait-for-emulator.sh
+++ android-wait-for-emulator.sh
@@ -10,8 +10,7 @@ timeout_in_sec=300 # 5 minutes
 
 until [[ "$bootanim" =~ "stopped" ]]; do
   bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
-  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
-    || "$bootanim" =~ "running" || "$bootanim" =~  "error: no emulators found" ]]; then
+  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline" || "$bootanim" =~ "running" || "$bootanim" =~  "error: no emulators found" ]]; then
     let "failcounter += 1"
     echo "Waiting for emulator to start"
     if [[ $failcounter -gt timeout_in_sec ]]; then
@@ -19,6 +18,7 @@ until [[ "$bootanim" =~ "stopped" ]]; do
       exit 1
     fi
   fi
+  echo "${failcounter} ==="
   sleep 1
 done
 
