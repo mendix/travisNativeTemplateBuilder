dc8fc51f2ff779196cf066779bfc39010334d6e7
mehmet <mehmet.izci@mendix.com>
2020-10-06 15:38:07 +0200

    try old working state

--- create-emulator.sh
+++ create-emulator.sh
@@ -2,11 +2,9 @@
 set -ex
 
 # Set up KVM on linux for hardware acceleration. Manually here so it only happens for emulator tests, takes ~30s
-#sudo -E apt-get -yq --no-install-suggests --no-install-recommends install bridge-utils libpulse0 libvirt-bin qemu-kvm virtinst ubuntu-vm-builder >/dev/null
-sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients >/dev/null
-sudo apt-get install bridge-utils
-sudo adduser "$USER" libvirt
-sudo adduser "$USER" kvm
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends install bridge-utils libpulse0 libvirt-bin qemu-kvm virtinst ubuntu-vm-builder >/dev/null
+sudo adduser $USER libvirt
+sudo adduser $USER kvm
 
 echo no | avdmanager create avd --force -n TEST_DEVICE -k "system-images;android-${COMPILE_API};google_apis;x86_64" -c 100M # create device
 sudo -E sudo -u "$USER" -E bash -c "emulator -avd TEST_DEVICE -verbose -no-snapshot -no-window -camera-back none -camera-front none -selinux permissive -qemu -m 2048 &"
